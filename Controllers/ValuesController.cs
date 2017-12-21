1f4a23fb90b22676203eb74a756729237664e276
Darrel Miller <darrmi@microsoft.com>
2017-12-20 22:09:14 -0500

    Initial Commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace feedApi.Controllers
+{
+    [Route("feed")]
+    public class FeedController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            FileStreamResult result;
+            using(var stream = this.GetType().Assembly.GetManifestResourceStream("feedApi.feed.xml")) {
+                 result =  new FileStreamResult(stream, "application/xml");
+            }
+            return result;
+        }
+    }
+}
